507907204babbb1d5767ab84ac497628904f1c61
khalid saif <k1s1saif@gmail.com>
2025-04-22 15:32:08 +0800

    test

---
+++ BaseVersion.Models/BusinessModels/Enums.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseVersion.Models.BusinessModels
+{
+    public enum EnumAction
+    {
+        Create,
+        Update,
+        Delete
+    }
+
+    public enum EnumStatus
+    {
+        Created,
+        Updated,
+        Deleted,
+        Approved,
+        Rejected,
+        PendingForApproval
+    }
+}
